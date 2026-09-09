6e1ae713ebd041d5c08e9e5ae78453449abb35da
Kevin Buchmann <k_buchmann@icloud.com>
2026-09-09 23:29:30 +0200

    Implement a function to write fetched HTML text into file

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
+import fs from 'node:fs';
 // Make HTTP request to the server at URL  https://memegen-link-examples-upleveled.netlify.app/
 import http from 'http';
 import https from 'https';
+import fetch from 'node-fetch';
 
-https.get('https://memegen-link-examples-upleveled.netlify.app/', (resp) => {
+const url = 'https://memegen-link-examples-upleveled.netlify.app/';
+
+// HTTPS approach
+
+https.get(url, (resp) => {
   let data = '';
 
   resp.on('data', (chunk) => {
+    console.log('chunk:', String(chunk));
     data += chunk;
   });
   resp.on('end', () => {
-    console.log(data);
+    fs.writeFile('./test.txt', data, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // file written successfully
+      }
+    });
   });
 });
+
+// Node-fetch module approach
+
+// const response = await fetch('url');
+// const body = await response.text();
+
+// console.log(body);
 
 // // Create a local server to receive data from
 // const server = http.createServer();
