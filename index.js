8ce6e1c842981c3e861b78111046b293219ab1cd
Kevin Buchmann <k_buchmann@icloud.com>
2026-09-10 21:55:04 +0200

    Implement the function to fetch image URLs

--- index.js
+++ index.js
@@ -4,27 +4,49 @@ import http from 'http';
 import https from 'https';
 import fetch from 'node-fetch';
 
-const url = 'https://memegen-link-examples-upleveled.netlify.app/';
+const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
+
+async function getMemes() {
+  // Like the browser fetch API, the default method is GET
+  const response = await fetch(memeUrl);
+  const data = await response.text();
+  fs.writeFile('./test.txt', data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // file written successfully
+    }
+  });
+  const pattern = /<img\s+src="(.+?)"\s+\/>/g;
+  const arr = data.matchAll(pattern);
+  const allMatches = Array.from(arr);
+  for (let i = 0; i < 10; i++) {
+    let firstTen = '';
+    firstTen += allMatches[i][1];
+    console.log(firstTen);
+  }
+}
+getMemes().catch(console.error);
 
 // HTTPS approach
 
-https.get(url, (resp) => {
-  let data = '';
+// https.get(url, (resp) => {
+//   let data = '';
 
-  resp.on('data', (chunk) => {
-    console.log('chunk:', String(chunk));
-    data += chunk;
-  });
-  resp.on('end', () => {
-    fs.writeFile('./test.txt', data, (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        // file written successfully
-      }
-    });
-  });
-});
+//   resp.on('data', (chunk) => {
+//     console.log('chunk:', String(chunk));
+//     data += chunk;
+//   });
+//   resp.on('end', () => {
+//     fs.writeFile('./test.txt', data, (err) => {
+//       if (err) {
+//         console.error(err);
+//       } else {
+//         // file written successfully
+//       }
+//     });
+//   });
+// });
 
 // Node-fetch module approach
 
