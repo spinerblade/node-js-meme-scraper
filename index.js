bb04bd6918f233d29ccd5bb7bb8f936519157250
Kevin Buchmann <k_buchmann@icloud.com>
2026-09-11 13:59:53 +0200

    Remove getMemes function and use named capturing groups

--- index.js
+++ index.js
@@ -8,27 +8,24 @@ const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
 fs.mkdirSync('./memes', { recursive: true });
 
 // Implement the HTTP request to get HTML body as a string first and then its URLs
-async function getMemes() {
+try {
   // HTTP request to meme page
   const responseHtml = await fetch(memeUrl);
   const htmlBody = await responseHtml.text(); // get whole HTML as string
 
-  const pattern = /<img\s+src="(.+?)"\s+\/>/g; // RegEx pattern that matches the image elements
+  const pattern = /<img\s+src="(?<url>.+?)"\s+\/>/g; // RegEx pattern that matches the image elements
   const imageElement = htmlBody.matchAll(pattern); // returns iterable of the image elements
   const allMatches = Array.from(imageElement); // convert iterable to array
   // Loop over array and download each URLs content into .jpg file
   for (let i = 0; i < 10; i++) {
-    // Just for testing
-    // let firstTen = '';
-    // firstTen += allMatches[i][1];
-    // console.log(firstTen);
-
-    const responseMeme = await fetch(allMatches[i][1]); // download image at URL
+    const url = allMatches[i].groups.url;
+    const responseMeme = await fetch(url); // download image at URL
     const arrayBuffer = await responseMeme.arrayBuffer(); // convert to array buffer
     const buffer = Buffer.from(arrayBuffer); // convert to buffer
     const indexString = (i + 1).toString().padStart(2, '0'); // convert index to name for .jpg (e.g. 01.jpg)
     const path = `./memes/${indexString}.jpg`;
     await fs.promises.writeFile(path, buffer); // save each image into folder "memes"
   }
+} catch (err) {
+  console.error(err); // error catching
 }
-getMemes().catch(console.error); // error catching
